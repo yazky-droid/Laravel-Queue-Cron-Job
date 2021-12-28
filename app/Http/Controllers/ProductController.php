9940824ab5b8bf087bdf223c0deec5a2d035dd02
aronei44 <meliodasmeliodas224@gmail.com>
2021-12-28 12:50:29 +0700

    setup product controller and s3

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json([
+            'message'=>'success',
+            'data'=>Product::all()
+        ],200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'product_name'=>'required',
+            'product_price'=>'required|numeric',
+            'image'=>'required|mimes:jpg,png,jpeg'
+        ]);
+        try {
+            $image = $request->image->store('images','s3');
+            $url = Storage::disk('s3')->url($image);
+
+            $product = Product::create([
+                'product_name'=>$request->product_name,
+                'product_price'=>$request->product_price,
+                'image_name'=>basename($image),
+                'image_url'=>$url
+            ]);
+            return response()->json([
+                'message'=>'success',
+                'data'=>$product
+            ],201);
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response()->json([
+                'message'=>'failed'
+            ],400);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        return response()->json([
+            'message'=>'success',
+            'data'=>$product
+        ],200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        try {
+            //code...
+            Storage::disk('s3')->delete($product->image_name);
+            $product->delete();
+            return response()->json([
+                'message'=>'success'
+            ],200);
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response()->json([
+                'message'=>'failed'
+            ],400);
+        }
+    }
+}
